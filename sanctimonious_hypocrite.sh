149ee09c1a7761eb9ae5290eab5116605da3d41e
boblsturm <bobs@kth.se>
2019-12-13 15:02:48 +0100

    updated script

--- sanctimonious_hypocrite.sh
+++ sanctimonious_hypocrite.sh
@@ -2,5 +2,5 @@
 	if [ "$1" = "Trump" ] || [ "$1" = "Trump sycophant" ]; then
 echo "Romans 13: Let everyone be subject to the governing authorities, for there is no authority except that which God has established. The authorities that exist have been established by God yada yada ..."
 else
-echo "Revelations 13: Elected official" $1 "is clearly the Devil!"
+echo "Revelations 13: $1 "is the Devil!"
 fi
